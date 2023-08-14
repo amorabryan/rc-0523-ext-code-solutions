4518810fac9e9fbab025ac4c6052bb5fe2b3855c
Bryan Amora <amorabryan@yahoo.com>
2023-08-14 22:01:21 +0000

    Added async function to takeAChance

--- es6-promises/main.js
+++ es6-promises/main.js
@@ -1,9 +1,20 @@
 import takeAChance from './take-a-chance.js';
 
-takeAChance('Bryan')
-  .then((success) => {
-    console.log(success);
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
+// takeAChance('Bryan')
+//   .then((success) => {
+//     console.log(success);
+//   })
+//   .catch((error) => {
+//     console.error(error.message);
+//   });
+
+async function fiftyChance() {
+  try {
+    const message = await takeAChance('Bryan');
+    console.log(message);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+  }
+}
+
+fiftyChance();
