e1d2f87ff79202c4876c41b8b4c8792945d65b1a
Bryan Amora <117689047+amorabryan@users.noreply.github.com>
2023-08-14 15:02:21 -0700

    Revert "Completed es6-promises"

--- es6-promises/main.js
+++
@@ -1,9 +0,0 @@
-import takeAChance from './take-a-chance.js';
-
-takeAChance('Bryan')
-  .then((success) => {
-    console.log(success);
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
